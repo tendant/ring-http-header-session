b94ab25b55736d53a013e349fa2f3f100c861577
Lei Wang <tendant@gmail.com>
2020-04-17 17:51:47 -0700

    fix incompatible code with wrap-session

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject tendant/ring-http-header-session "0.1.1-SNAPSHOT"
+(defproject tendant/ring-http-header-session "0.1.1"
   :description "Similar to ring session middleware, using http header mainly for mobile apps, instead of http cookie."
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
